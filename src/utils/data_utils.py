0707f6b97fe027f754392b36a5e494a6df4171a1
aadriien <adriienlynch@gmail.com>
2025-03-28 16:53:18 -0400

    Developed helper for CSV export & file path check

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -11,16 +11,21 @@ import pandas as pd
 from typing import Optional
 
 
-def csv_exists(csv_file: str) -> bool:
+# Check valid directory (with option to create if doesn't exist)
+def folder_exists(csv_file: str, create: bool = False) -> bool:
     # Extract folder in file path
     folder = os.path.dirname(csv_file)  
-    
-    if not os.path.exists(folder):
+
+    if create and not os.path.exists(folder):
         print(f"Folder '{folder}' not found. Creating it...")
         os.makedirs(folder)
 
-    if os.path.exists(csv_file):
-        return True
+    return os.path.exists(folder)
+
+
+def csv_exists(csv_file: str) -> bool:
+    if folder_exists(csv_file):
+        return os.path.exists(csv_file)
 
     return False
 
@@ -34,6 +39,22 @@ def read_csv(csv_file: str, columns: Optional[list[str]] = None) -> pd.DataFrame
 
     return pd.read_csv(csv_file, usecols=columns)
 
+
+def export_to_csv(csv_file: str, dataframe: pd.DataFrame) -> None:
+    if not csv_file:
+        raise ValueError("Error, must specify CSV file path")
+
+    if dataframe is None or not isinstance(dataframe, pd.DataFrame):
+        raise ValueError("Error, must specify a valid DataFrame to export")
+
+    if dataframe.empty:
+        raise ValueError("Error, must specify a non-empty DataFrame to export")
+
+    # Proceed with creating folder if doesn't exist, then export CSV
+    _ = folder_exists(csv_file, True)
+    dataframe.to_csv(csv_file, index=False)
+
+    print(f"Exported {len(dataframe)} entries to {csv_file}")
 
 
 
