a0e67812f0524e388c91a83534975d7e1a505cc5
aadriien <adriienlynch@gmail.com>
2025-04-03 14:36:27 -0400

    Added country_coverter library for ISO codes not mapped to full country names in dataset

--- src/config.py
+++ src/config.py
@@ -5,9 +5,24 @@
 ###############################################################################
 
 
+import country_converter as coco
+
+
 COMMON_NAME = "whale shark"
 SPECIES_NAME = "rhincodon typus"
 
 MONTH_NAMES = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", 
                 "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+
+def convert_ISO_code_to_country(country_code: str) -> str:
+    if not isinstance(country_code, str):
+        raise ValueError("Error, must specify country_code")
+
+    if country_code in ["ZZ", "XX", None, ""]:
+        return "Unknown"
+
+    full_country_name = coco.convert(names=country_code, to="name")
+    return full_country_name
+
 
