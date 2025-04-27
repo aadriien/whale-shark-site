f9c317a3d467720a72ce65b3f2df33aa35f14cac
aadriien <adriienlynch@gmail.com>
2025-04-27 15:10:28 -0400

    Updates

--- assets/SharkTracker-D4if5B3m.js
+++ assets/SharkTracker-D4if5B3m.js
@@ -1,4 +1,4 @@
-import{r as d,j as e,L as g}from"./index-Acynp4MZ.js";import{G as k}from"./Globe-Cl5mT4dI.js";import"./three.module-B1l304HR.js";import"./linear-D8wwPlEk.js";const w=({shark:a,onPlayStory:n,isPlaying:l,playingSharkId:r})=>{const[t,s]=d.useState(!1),c=()=>{s(!t)};return e.jsxs("div",{className:`shark-card
+import{r as d,j as e,L as g}from"./index-BLYUdxF6.js";import{G as k}from"./Globe-Dsm0UIln.js";import"./three.module-B1l304HR.js";import"./linear-D8wwPlEk.js";const w=({shark:a,onPlayStory:n,isPlaying:l,playingSharkId:r})=>{const[t,s]=d.useState(!1),c=()=>{s(!t)};return e.jsxs("div",{className:`shark-card
             ${r===a.id&&l?" currentlyPlaying":""}
             ${l?" anyPlaying":""}
             ${t?" expanded":""}
