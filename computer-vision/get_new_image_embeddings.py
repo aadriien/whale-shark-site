2ec6b67a416b3283644e95e92897b111707aee8c
aadriien <adriienlynch@gmail.com>
2025-04-30 16:03:56 -0400

    Added Ultralytics for YOLOv8 model (image AI object detection BBOX)

---
+++ computer-vision/get_new_image_embeddings.py
@@ -0,0 +1,17 @@
+###############################################################################
+##  `get_new_image_embeddings.py`                                            ##
+##                                                                           ##
+##  Purpose: Calculates bounding box & model-outputted embedding per image   ##
+###############################################################################
+
+
+
+# Step 1: Open gbif_media.csv & read all entries (grab images)
+
+# Step 2: Use YOLOv8 model to detect shark object & create bounding box
+
+# Step 3: Use Hugging Face MiewID-msv3 model to generate image embedding 
+
+
+
+
