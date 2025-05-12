0ad4446c1502f014ceea50739aef96cf55abff4f
aadriien <adriienlynch@gmail.com>
2025-05-12 16:23:51 -0400

    Began validation of media matches per occurrence

--- computer-vision/match_embeddings.py
+++ computer-vision/match_embeddings.py
@@ -12,7 +12,11 @@ from typing import Tuple
 
 
 from src.utils.data_utils import (
-    export_to_csv,
+    read_csv, export_to_csv,
+)
+
+from src.analyze.gbif import (
+    GBIF_INDIVIDUAL_SHARKS_STATS_CSV,
 )
 
 from .process_annotations import (
@@ -26,6 +30,7 @@ from .get_new_image_embeddings import (
 
 
 GBIF_MEDIA_MATCHES_FILE = f"{NEW_EMBEDDINGS_FOLDER}/GBIF_media_matches.csv"
+GBIF_INDIVIDUAL_MATCHES_FILE = f"{NEW_EMBEDDINGS_FOLDER}/GBIF_shark_matches.csv"
 
 
 # L2 DISTANCE SCALE (for normalized values, to judge match likelihood): 
@@ -71,7 +76,7 @@ def identify_sharks(known_data: dict, new_data: dict) -> list[dict]:
         matched_idx = idx[0]
         result = {
             "query_index": i,
-            "closest_whale_shark_name": whale_shark_names[matched_idx],
+            "closest_whale_shark_id": whale_shark_names[matched_idx],
             "matched_image_id": image_ids[matched_idx],
             "matched_annotation_id": annotation_ids[matched_idx],
             "distance": round(float(dist[0]), 4)
@@ -79,6 +84,50 @@ def identify_sharks(known_data: dict, new_data: dict) -> list[dict]:
         results.append(result)
 
     return results
+
+
+
+def validate_matches(media_matches_df: pd.DataFrame) -> None:
+    media_sharks_df = read_csv(GBIF_INDIVIDUAL_SHARKS_STATS_CSV)
+
+    RELEVANT_COLUMNS = [
+        "whaleSharkID",
+        # "organismID",
+        "identificationID",
+        "Oldest Occurrence",
+        "Newest Occurrence",
+        "country (year)",
+        "stateProvince - verbatimLocality (month year)",
+        # "imageURL (license, creator)"
+    ]
+    media_sharks_df = media_sharks_df[RELEVANT_COLUMNS]
+    individual_sharks = media_sharks_df.dropna(subset=RELEVANT_COLUMNS).copy()
+    individual_sharks.reset_index(drop=True, inplace=True)
+
+
+    format_str = "closest_whale_shark_id: {0} (matched_image_id: {1}, distance: {2})"
+    column_name = "closest_whale_shark_id (matched_image_id, distance)"
+    all_metric_vals = [
+        "closest_whale_shark_id",
+        "matched_image_id",
+        "distance"
+    ]
+
+    individual_sharks["identificationID"] = individual_sharks["identificationID"].astype(str)
+    media_matches_df["identificationID"] = media_matches_df["identificationID"].astype(str)
+
+    media_matches_df = media_matches_df.groupby("identificationID").apply(
+        lambda x: ", ".join(sorted(set(
+            format_str.format(*vals) 
+            for vals in 
+            zip(*(x[col] for col in all_metric_vals))
+        ))), 
+        include_groups=False
+    ).reset_index(name=column_name)
+
+    individual_sharks = individual_sharks.merge(media_matches_df, on="identificationID", how="left")
+
+    export_to_csv(GBIF_INDIVIDUAL_MATCHES_FILE, individual_sharks)
 
 
 
@@ -120,5 +169,7 @@ if __name__ == "__main__":
 
 
     export_to_csv(GBIF_MEDIA_MATCHES_FILE, enriched_df)
+
+    validate_matches(enriched_df)
 
 
