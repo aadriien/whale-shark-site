9e920f35a425465fb4d93ef7e83e357559fa3c05
aadriien <adriienlynch@gmail.com>
2025-07-04 14:30:59 -0400

    Added static image for whale shark habitat map overview

--- website/src/components/overviews/HabitatMap.jsx
+++ website/src/components/overviews/HabitatMap.jsx
@@ -4,7 +4,7 @@ const HabitatMap = () => {
             <h2>Where Do Whale Sharks Live?</h2>
             
             <div className="map-container">
-                <img src="/images/world-map.png" alt="Whale Shark Habitat Map" />
+                <img src="./habitat-map.png" alt="Whale Shark Habitat Map" />
             </div>
             <p>[Short description about regions they inhabit]</p>
         </section>
