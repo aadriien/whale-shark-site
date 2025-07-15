4074b0e81a5ee7140aee2965477b4f9d7003c74c
aadriien <adriienlynch@gmail.com>
2025-07-15 15:12:42 -0400

    Made navbar mobile-friendly

--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
@@ -6,42 +5,20 @@ function Navbar() {
     const location = useLocation(); 
     
     return (
-        <nav style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#333",
-            padding: "15px 15px",
-            position: "fixed",
-            top: 0,
-            width: "100%",
-            zIndex: 10
-        }}>
-            {/* Links */}
-            <Link to="/home" style={getLinkStyle(location.pathname, "/home")}>Home</Link>
-            <Link to="/about" style={getLinkStyle(location.pathname, "/about")}>About</Link>
-            <Link to="/globeviews" style={getLinkStyle(location.pathname, "/globeviews")}>Globe Views</Link>
-            <Link to="/sharktracker" style={getLinkStyle(location.pathname, "/sharktracker")}>Shark Tracker</Link>
-            <Link to="/datavisuals" style={getLinkStyle(location.pathname, "/datavisuals")}>Data Visuals</Link>
-            <Link to="/environment" style={getLinkStyle(location.pathname, "/environment")}>Environment</Link>
-            <Link to="/buildashark" style={getLinkStyle(location.pathname, "/buildashark")}>Build-A-Shark</Link>
-            <Link to="/animation" style={getLinkStyle(location.pathname, "/animation")}>Animation</Link>
+        <nav>
+            <Link to="/home" className={location.pathname === "/home" ? "active" : ""}>Home</Link>
+            <Link to="/about" className={location.pathname === "/about" ? "active" : ""}>About</Link>
+
+            <Link to="/globeviews" className={location.pathname === "/globeviews" ? "active" : ""}>Globe Views</Link>
+            <Link to="/sharktracker" className={location.pathname === "/sharktracker" ? "active" : ""}>Shark Tracker</Link>
+            <Link to="/datavisuals" className={location.pathname === "/datavisuals" ? "active" : ""}>Data Visuals</Link>
+            <Link to="/environment" className={location.pathname === "/environment" ? "active" : ""}>Environment</Link>
+            <Link to="/buildashark" className={location.pathname === "/buildashark" ? "active" : ""}>Build-A-Shark</Link>
+            <Link to="/animation" className={location.pathname === "/animation" ? "active" : ""}>Animation</Link>
         </nav>
     );
 }
     
-// Apply active styles based on current page location
-const getLinkStyle = (currentPath, linkPath) => {
-    return {
-        margin: "0 25px",
-        fontSize: "18px",
-        textDecoration: "none",
-        color: currentPath === linkPath ? "#fff41d" : "#fff",  // Active page gets yellow
-        fontWeight: currentPath === linkPath ? "bold" : "normal", // Bold active link
-        // transition: "color 0.1s ease",
-    };
-}
-
 export default Navbar;
 
     
