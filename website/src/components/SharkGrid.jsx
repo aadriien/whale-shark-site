61b053e30086972c4963f3ae7d7fcbaf2074115a
aadriien <adriienlynch@gmail.com>
2025-08-08 15:22:40 -0400

    Fixed indentation for consistency

--- website/src/components/SharkGrid.jsx
+++ website/src/components/SharkGrid.jsx
@@ -1,18 +1,17 @@
-import React from "react";
 import SharkCard from "./SharkCard";
 
 const SharkGrid = ({ sharks, onPlayStory, isPlaying, playingSharkId }) => {
-  return (
-    <div className="shark-grid">
-      {sharks.map((shark) => (
-        <SharkCard 
-            key={shark.id} shark={shark} 
-            onPlayStory={onPlayStory} isPlaying={isPlaying} 
-            playingSharkId={playingSharkId}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div className="shark-grid">
+            {sharks.map((shark) => (
+                <SharkCard 
+                    key={shark.id} shark={shark} 
+                    onPlayStory={onPlayStory} isPlaying={isPlaying} 
+                    playingSharkId={playingSharkId}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default SharkGrid;
