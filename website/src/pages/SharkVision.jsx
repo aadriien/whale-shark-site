52aa1f75f7694cd3ddb1fb436e40375d395d34ef
aadriien <adriienlynch@gmail.com>
2025-09-26 16:55:28 -0400

    Integrated newly-prototyped components into page with CSS rules

--- website/src/pages/SharkVision.jsx
+++ website/src/pages/SharkVision.jsx
@@ -1,8 +1,19 @@
+import CVHeroIntro from "../components/overviews/CVHeroIntro.jsx";
+import CVPipeline from "../components/overviews/CVPipeline.jsx";
+import InteractiveDemo from "../components/overviews/InteractiveDemo.jsx";
+import CVApplications from "../components/overviews/CVApplications.jsx";
+
+
 function SharkVision() {
     return (
         <div className="page-content shark-vision-wrapper">
-            <h1>Whale Shark Computer Vision</h1>
-
+            {/* <h1>Whale Shark Computer Vision</h1> */}
+            
+            <CVHeroIntro />
+            <CVPipeline />
+            <InteractiveDemo />
+            <CVApplications />
+        
         </div>
     );
 }
