8ae0a0510c871ee27f0b70477385f33ff55dffaf
aadriien <adriienlynch@gmail.com>
2025-11-23 14:15:47 -0500

    Began outlining types for charts & also specified SVG import declarations

---
+++ website/src/types/assets.d.ts
@@ -0,0 +1,8 @@
+/* Data module (declaration) types */
+
+declare module "*.svg" {
+    const src: string;
+    export default src;
+}
+
+
---
+++ website/src/types/charts.ts
@@ -0,0 +1,12 @@
+/* Chart types */
+
+export type ChartPlaceholderProps = { 
+    type: string; 
+    message: string 
+};
+
+export type ImageMap = { 
+    [key: string]: string
+};
+
+
