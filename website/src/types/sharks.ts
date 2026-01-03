b82709eb53731f871966a4279bc7319f70f5a62b
aadriien <adriienlynch@gmail.com>
2026-01-03 13:45:00 -0500

    Outlined new types for whale shark filtering by selected criteria

--- website/src/types/sharks.ts
+++ website/src/types/sharks.ts
@@ -125,3 +125,31 @@ export type IndividualSharkProps = {
 };
 
 
+export type SharkBaseCriteria = {
+    country: string;
+    publishingCountry: string;
+
+    yearRange: [string, string];
+    month: string;
+
+    sex: string;
+    lifeStage: string;
+
+    minRecords: number;
+    observationType: string;
+
+    showOnlyWithMedia: boolean;
+    hasOccurrenceNotes: boolean;
+};
+
+export type SharkMatchCriteria = SharkBaseCriteria & {
+    hasMatchedImages: boolean;
+    miewidDistanceRange: [number, number];
+
+    showOnlyConfidentMatches: boolean;
+    plausibility: string;
+};
+
+export type SharkCriteria = SharkBaseCriteria | SharkMatchCriteria;
+
+
