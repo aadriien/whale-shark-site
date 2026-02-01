881acc2987afa700ad148523bea00fd1d6f3fd1d
aadriien <adriienlynch@gmail.com>
2026-02-01 14:36:04 -0500

    Updates

--- assets/PlayStoryButton-D968o_EY.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./index-DpL8moQh.js";const d=({shark:o,onPlayStory:r,isPlaying:e,playingSharkId:l,showPauseForGeoLabs:i=!1,onToggleStepMode:a,isStepMode:s=!1})=>o?i?t.jsx("div",{className:"play-story-section",children:t.jsx("button",{className:`geo-labs-step-button
-                        ${s?" step-mode-active":""}
-                    `,onClick:n=>{n.stopPropagation(),a()},children:s?"Exit Story Mode":t.jsxs(t.Fragment,{children:["Step Through ",t.jsx("strong",{children:o.name||o.id}),"'s Story"]})})}):t.jsx("div",{className:"play-story-section",children:t.jsx("button",{className:`play-story-button
-                    ${l===o.id&&e?" currentlyPlaying":""}
-                    ${e?" anyPlaying":""}
-                `,onClick:n=>{n.stopPropagation(),r(o.id)},disabled:e,children:e?"Story in Progress...":t.jsxs(t.Fragment,{children:["Play ",t.jsx("strong",{children:o.name||o.id}),"'s Story"]})})}):null;export{d as P};
