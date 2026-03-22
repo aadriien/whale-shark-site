179efe729df149263c054f0893638d27a1f581d6
aadriien <adriienlynch@gmail.com>
2026-03-22 15:15:04 -0400

    Cleaned up types nested within GlobeHandler (+ added support for a few more)

--- website/src/types/globes.ts
+++ website/src/types/globes.ts
@@ -1,30 +1,61 @@
 import ThreeGlobe from "three-globe";
+import { WhaleSharkEntryNormalized, WhaleSharkDatasetNormalized } from "./sharks";
 import { PlottedCoordinatePoint } from "./coordinates";
 
 /* Globes types */
 
+export type GetGlobeFn = () => ThreeGlobe;
+
+export type PlayStoryFn = (
+    sharkID: string,
+    onPointChange?: (point: PlottedCoordinatePoint) => void
+) => Promise<void>;
+
+export type PlayStoryFromSelectionFn = PlayStoryFn;
+
+export type HighlightSharkFn = (
+    sharkID: string, 
+    usePoints?: boolean, 
+    keepControlsDisabled?: boolean
+) => Promise<void>;
+
+export type InterruptStoryFn = () => void;
+
+export type ShowSinglePointFn = (
+    point: PlottedCoordinatePoint, 
+    disableControls?: boolean
+) => void;
+
+export type EnableDisableControlFn = () => void;
+
 export type GlobeHandle = {
-    getGlobe: () => ThreeGlobe;
-    playStory: (
-        sharkID: string, 
-        onPointChange?: (point: PlottedCoordinatePoint) => void
-    ) => Promise<void>;
-    playStoryFromSelection: (
-        sharkID: string, 
-        onPointChange?: (point: PlottedCoordinatePoint) => void
-    ) => Promise<void>;
-    highlightShark: (
-        sharkID: string, 
-        usePoints?: boolean, 
-        keepControlsDisabled?: boolean
-    ) => Promise<void>;
-    interruptStory: () => void;
-    showSinglePoint: (
-        point: PlottedCoordinatePoint, 
-        disableControls?: boolean
-    ) => void;
-    enableControls: () => void;
-    disableControls: () => void;
+    getGlobe: GetGlobeFn;
+    playStory: PlayStoryFn;
+    playStoryFromSelection: PlayStoryFromSelectionFn;
+    highlightShark: HighlightSharkFn;
+    interruptStory: InterruptStoryFn;
+    showSinglePoint: ShowSinglePointFn;
+    enableControls: EnableDisableControlFn;
+    disableControls: EnableDisableControlFn;
+};
+
+
+export type UseGlobeClickParams = {
+    sharks: WhaleSharkDatasetNormalized;
+    pointsData: PlottedCoordinatePoint[];
+    allSharksVisible: boolean;
+    onSharkSelect: (shark: WhaleSharkEntryNormalized) => void;
+};
+
+
+export type SharkClickParams = { 
+    lat: number, 
+    lng: number
+};
+
+export type GlobeProps = {
+    onSharkClick: (sharkClickParams: SharkClickParams) => void;
+    allowClicks: boolean;
 };
 
 
